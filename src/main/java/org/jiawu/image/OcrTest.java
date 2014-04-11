a21e17b906efa274c38460072cdb2be1a437024b
jiawuwu <jiawu.wu@opi-corp.com>
2014-04-11 17:58:39 +0800

    图片验证码识别

---
+++ src/main/java/org/jiawu/image/OcrTest.java
@@ -0,0 +1,22 @@
+package org.jiawu.image;
+
+import java.io.File;
+import java.io.IOException;
+
+public class OcrTest {
+
+    public static void main(String[] args) {
+        String path = "/data/temp/captcha.jpg";
+        System.out.println("ORC Test Begin......");
+        try {
+            String valCode = new OCR().recognizeText(new File(path), "png");
+            System.out.println(valCode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("ORC Test End......");
+    }
+
+}
