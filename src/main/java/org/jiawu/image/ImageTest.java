d44709e4d693c2ae6f215963bc5c64777a3ccc8f
jiawuwu <jiawu.wu@opi-corp.com>
2014-07-11 15:14:24 +0800

    test

---
+++ src/main/java/org/jiawu/image/ImageTest.java
@@ -0,0 +1,12 @@
+package org.jiawu.image;
+
+/**
+ * Created by jiawuwu on 14-5-26.
+ */
+public class ImageTest {
+
+    public static void main(String[] args){
+        String path = "/data/temp/captcha.png";
+
+    }
+}
