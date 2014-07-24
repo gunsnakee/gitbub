c2b52a1349f690de16b509aeb0134f4a17e4ec2a
jiawuwu <jiawu.wu@opi-corp.com>
2014-07-24 15:31:54 +0800

    test

---
+++ src/main/java/org/jiawu/main/MainTest.java
@@ -0,0 +1,13 @@
+package org.jiawu.main;
+
+/**
+ * Created by jiawuwu on 14-7-23.
+ */
+public class MainTest {
+
+    public static void main(String[] args){
+        for(int i=1;i<=100;i++){
+            System.out.println("mlb"+(1000+i)+"@meiliwan.com mlb2014mlb"+(1000+i)+" abcd1234");
+        }
+    }
+}
