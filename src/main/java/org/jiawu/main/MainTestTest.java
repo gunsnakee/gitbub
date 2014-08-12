f392fbd34c2a27463ab1bcb22684399f43c00c84
jiawuwu <jiawu.wu@opi-corp.com>
2014-08-12 15:15:50 +0800

    test

---
+++ src/main/java/org/jiawu/main/MainTestTest.java
@@ -0,0 +1,16 @@
+package org.jiawu.main;
+
+/**
+ * Created by jiawuwu on 14-8-6.
+ */
+public class MainTestTest {
+    @org.testng.annotations.BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+}
--- web/src/main/java/com/wujw/web/controller/ThymeleafTestController.java
+++
@@ -1,8 +0,0 @@
-package com.wujw.web.controller;
-
-/**
- * Created by jiawuwu on 14-8-5.
- */
-@Controller
-public class ThymeleafTestController {
-}
